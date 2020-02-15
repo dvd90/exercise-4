783c96e452c31850358420199cc171bd7bd97843
David Sellam <dvdsellam@gmail.com>
2020-02-15 16:37:22 +0200

    first deploy

---
+++ app/precache-manifest.c6c72c32471703a9b61cfd4def86a96d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da2af8819d58554520c8b8f73dcb7dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94d7da422aa47b839e7",
+    "url": "/static/css/main.860c63ee.chunk.css"
+  },
+  {
+    "revision": "34c456a08889e165a9fc",
+    "url": "/static/js/2.ea5b3d25.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ea5b3d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94d7da422aa47b839e7",
+    "url": "/static/js/main.e5842018.chunk.js"
+  },
+  {
+    "revision": "3b9eb268cd8e0bbc2c71",
+    "url": "/static/js/runtime-main.fb27dc5f.js"
+  },
+  {
+    "revision": "f867380825a2c8ea6efb8c5e61ca159e",
+    "url": "/static/media/background.f8673808.png"
+  }
+]);
